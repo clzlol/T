5912c1da1a09badfe449a2edd42aad91f9388897
clzlol <nyc0527@naver.com>
2022-08-15 09:38:22 +0900

    1

---
+++ h/src/Variable.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Variable {
+    public static void main(String[] args) {
+        
+        Scanner s = new Scanner(System.in);
+
+        String name=args[0];
+        String city=args[1];
+        System.out.println(name + " "+city);
+
+    }
+
+
+}
+
